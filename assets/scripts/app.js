eff899e06135c2795fae06fb0905cf7cb6cae1d8
Max Xu <sixu@cisco.com>
2017-06-29 14:39:15 +0800

    update for word.

---
+++ assets/scripts/app.js
@@ -0,0 +1,57 @@
+$.get("assets/scripts/data.json", function (data) {
+    var vm = new Vue({
+        "el": "#app",
+        "data": {
+            "selected": {
+                "word": {},
+                "examples": false,
+                "en": false,
+                "cn": false,
+                "detail": false
+            }
+        },
+        "methods": {
+            "recall": function (words, selected) {
+                var index = Math.min(words.length, selected);
+                var word = words[index];
+                this.selected = Object.assign({}, this.selected,
+                    {
+                        "word": data.words[word],
+                        "examples": false,
+                        "en": false,
+                        "cn": false,
+                        "detail": false
+                    });
+            },
+            "play": function (audioUrl) {
+                $('#audio').attr({
+                    "src": audioUrl,
+                    "autoplay": "autoplay"
+                });
+            },
+            "unknown": function () {
+                if (!this.selected.examples) {
+                    this.selected.examples = true;
+                    return;
+                }
+                if (!this.selected.en) {
+                    this.selected.en = true;
+                    return;
+                }
+                if (!this.selected.cn) {
+                    this.selected.cn = true;
+                    return;
+                }
+            },
+            "known": function () {
+                Object.assign(this.selected, {
+                    "examples": true,
+                    "en": true,
+                    "cn": true,
+                    "detail": true
+                });
+            }
+        }
+    });
+    vm.recall(["abandon"], 0);
+});
